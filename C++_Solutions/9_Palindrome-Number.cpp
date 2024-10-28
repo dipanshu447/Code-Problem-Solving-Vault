1ec3b09f815c2149d3ff85d4078cf6fe30d3f6b3
dipanshu447 <dipanshusahu447@gmail.com>
2024-10-28 17:32:17 +0530

    9. Palindrome Number

---
+++ C++_Solutions/9_Palindrome-Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        string rev;
+        for (int i = num.size()-1; i >= 0; i--)
+        {
+            rev += num[i]; 
+        }
+        int i = num.size();
+        cout << num << " == "<< rev << endl;
+        if (num.compare(rev) == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
