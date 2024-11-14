a9c9dac9be25bb3192b77eb7e8c9e7967c1a7347
dipanshu447 <dipanshusahu447@gmail.com>
2024-11-14 15:40:47 +0530

    small fix

--- C++_Solutions/70_Climbing-Stairs.cpp
+++ C++_Solutions/70_Climbing-Stairs.cpp
@@ -10,7 +10,7 @@ public:
         int first = 1;
         int second = 2;
         int current = 0;
-        for(int i = 3; i <=   n; i++){
+        for(int i = 3; i <= n; i++){
             current = first + second;
             first = second;
             second = current;
