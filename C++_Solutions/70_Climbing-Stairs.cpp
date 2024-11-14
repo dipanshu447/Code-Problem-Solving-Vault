c2e28b01aa41843484e4dc2bb0682731eedae15c
dipanshu447 <dipanshusahu447@gmail.com>
2024-11-14 15:55:11 +0530

    use less space to execute

--- C++_Solutions/70_Climbing-Stairs.cpp
+++ C++_Solutions/70_Climbing-Stairs.cpp
@@ -4,14 +4,13 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1){
+        if(n <= 2){
             return n;
         }
         int first = 1;
         int second = 2;
-        int current = 0;
-        for(int i = 3; i <= n; i++){
-            current = first + second;
+        for(int i = 3; i <=   n; i++){
+            int current = first + second;
             first = second;
             second = current;
         }
