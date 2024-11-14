4a061840f9421d1dbee21d598e9aa0b62769d6a2
dipanshu447 <dipanshusahu447@gmail.com>
2024-11-14 15:37:17 +0530

    Solved Climbing Stairs problem

---
+++ C++_Solutions/70_Climbing-Stairs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1){
+            return n;
+        }
+        int first = 1;
+        int second = 2;
+        int current = 0;
+        for(int i = 3; i <=   n; i++){
+            current = first + second;
+            first = second;
+            second = current;
+        }
+        return second;
+    }
+};
