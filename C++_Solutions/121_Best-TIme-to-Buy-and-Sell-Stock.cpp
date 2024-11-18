57c8de818c648fc950731a61c706da3e5d1aa296
dipanshu447 <dipanshusahu447@gmail.com>
2024-11-18 14:36:47 +0530

    Solved Best Time to buy and sell stock Problem

---
+++ C++_Solutions/121_Best-TIme-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int profit = 0;
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+};
