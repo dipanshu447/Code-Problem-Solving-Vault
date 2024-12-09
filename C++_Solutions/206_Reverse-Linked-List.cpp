9ede78938eed3420a22438df1736360566db20d3
dipanshu447 <dipanshusahu447@gmail.com>
2024-12-09 09:41:25 +0530

    Solved Reverse Linked List problem

---
+++ C++_Solutions/206_Reverse-Linked-List.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+        ListNode *prev = nullptr;
+        while(head != nullptr){
+            ListNode *NextNode = head->next; // Save the next node
+            head->next = prev; // reverse the link
+            prev = head; // move prev node forward
+            head = NextNode; // move head forward
+        }
+        return prev; // prev becomes the new head
+    }
+};
