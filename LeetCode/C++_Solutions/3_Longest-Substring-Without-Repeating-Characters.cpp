88e9cd424f12216855dc6cdc0ef387d4c73f3284
dipanshu447 <dipanshusahu447@gmail.com>
2024-12-20 12:20:19 +0530

    Found a even more efficient solution

--- LeetCode/C++_Solutions/3_Longest-Substring-Without-Repeating-Characters.cpp
+++ LeetCode/C++_Solutions/3_Longest-Substring-Without-Repeating-Characters.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
@@ -22,6 +23,25 @@ public:
             if(subStr.size() > maxlen){
                 maxlen = subStr.size();
             }
+        }
+        return maxlen;
+    }
+};
+
+// Even more efficient solution using vector index as ASCII values. 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen = 0, left = 0;
+        vector<int> lastIndex(256,-1); // Fixed-size array for ASCII characters
+        for (int right = 0; right < s.size(); ++right) {
+            // If the character was seen and is within the current substring
+            if (lastIndex[s[right]] >= left) {
+                left = lastIndex[s[right]] + 1; // Move the left pointer forward
+            }
+            maxlen = max((right - left) + 1, maxlen);
+            // Update the last-seen index for the current character
+            lastIndex[s[right]] = right;
         }
         return maxlen;
     }
