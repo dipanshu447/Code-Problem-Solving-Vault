13311416bcd00814adfd823bfd79343aea74f12a
dipanshu447 <dipanshusahu447@gmail.com>
2025-01-07 10:12:01 +0530

    Solved LCM and GCD problem

---
+++ GeekForGeeks/C++_Solutions/LCM-And-GCD.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    int gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    int lcm(int a, int b)
+    {
+        return (a * b) / gcd(a, b);
+    }
+
+public:
+    vector<int> lcmAndGcd(int a, int b)
+    {
+        return {lcm(a, b), gcd(a, b)};
+    }
+};
