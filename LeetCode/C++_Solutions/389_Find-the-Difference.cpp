82dd6f8efa11456000f70ffd768047fd8048f371
dipanshu447 <dipanshusahu447@gmail.com>
2025-02-14 10:03:44 +0530

    Solved Find the difference problem

---
+++ LeetCode/C++_Solutions/389_Find-the-Difference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int res = 0;
+        for(char c : s) res ^= c;
+        for(char c : t) res ^= c;
+        return res;
+    }
+};
