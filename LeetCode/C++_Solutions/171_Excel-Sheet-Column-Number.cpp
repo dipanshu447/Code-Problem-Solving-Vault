6cdd74f4efb70fbea2d4a5398f123323589a7d38
dipanshu447 <dipanshusahu447@gmail.com>
2025-03-03 10:42:24 +0530

    Solved Excel Sheet Column Number problem

---
+++ LeetCode/C++_Solutions/171_Excel-Sheet-Column-Number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res = 0;
+        for (char c : columnTitle) res = res * 26 + (c  - 'A' + 1);
+        return res;
+    }
+};
