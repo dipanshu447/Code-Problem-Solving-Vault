f24ecf3c0ca1fa09b4358d3aba5e5d3bc87c305a
Dipanshu Sahu <66827904+dipanshu447@users.noreply.github.com>
2025-03-26 16:59:02 +0530

    Solved Chunk-Array Problem

---
+++ LeetCode/30_Days_of_JavaScript/JSON/2677_Chunk-Array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const res = [];
+    for(let i = 0; i < arr.length; i += size) res.push(arr.slice(i,i + size));
+    return res;
+};
+// you can use lodash's _.chunk function. But thats cheating lol
